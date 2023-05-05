f15a510af69689ac503ac227527daae460d9c422
zpz <zepu.zhang@gmail.com>
2023-05-04 21:18:55 -0700

    minor

--- tests/test_deprecated_imports.py
+++ tests/test_deprecated_imports.py
@@ -1,17 +1,29 @@
-# noqa: I001, F401, F801
-
 import pytest
 
 
 def test_deprecated():
-    from mpservice.util import (
-        ProcessPoolExecutor, ThreadPoolExecutor, get_shared_process_pool, get_shared_thread_pool,
-        MP_SPAWN_CTX, Process, RemoteException, RemoteTraceback, SpawnProcess, get_remote_traceback, is_remote_exception,
-        get_docker_host_ip, is_async,
-        MAX_THREADS, Thread,
-        SpawnProcessPoolExecutor,
-    )
+    with pytest.warns(DeprecationWarning):
+        from mpservice.util import (
+            ProcessPoolExecutor,
+            ThreadPoolExecutor,
+            get_shared_process_pool,
+            get_shared_thread_pool,
+            MP_SPAWN_CTX,
+            Process,
+            RemoteException,
+            RemoteTraceback,
+            SpawnProcess,
+            get_remote_traceback,
+            is_remote_exception,
+            get_docker_host_ip,
+            is_async,
+            MAX_THREADS,
+            Thread,
+            SpawnProcessPoolExecutor,
+        )
+        assert True
 
     with pytest.warns(DeprecationWarning):
         from mpservice.server_process import Manager
+
         assert True
