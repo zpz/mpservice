9c0017a2f12d4c1a269432c58b5cef5647aac9ce
zpz <zepu.zhang@gmail.com>
2023-05-22 00:18:37 -0700

    doc

--- src/mpservice/streamer.py
+++ src/mpservice/streamer.py
@@ -1970,35 +1970,59 @@ def tee(instream: Iterable, n: int = 2, /, *, buffer_size: int = 256) -> tuple[S
 
     There are a few ways we can do this:
 
-        1. Revise the code to apply multiple operations on each data element, that is, "merging"
-           the two lines of operations. This likely make the code more complex. It could be even
-           hard.
+    1. Revise the code to apply multiple operations on each data element, that is, "merging"
+       the two lines of operations. This will likely make the code more complex.
 
-        2. Apply the first line of operations. Then re-walk the data stream to apply the second
-           line of operations. There's one scenario in which this approach is not good. In that scenario,
-           getting the data stream is expensive (for example, each element is obtained via an HTTP call).
-           There are also stituations where it's not possible to get the data stream a second time.
+    2. Apply the first line of operations; then re-walk the data stream to apply the second
+       line of operations. When feasible, this is simple and clean. However, this approach could
+       be infeasible, for example, when walking the data stream is expensive.
+       There are also stituations where it's not possible to get the data stream a second time.
 
-        3. Use the current function. The setup is like this::
+    3. Use the current function::
 
-            data = [...]
-            stream_1, stream_2 = teee(data, 2)
-            stream_1 = Stream(data).map(...).batch(...).parmap(...)...
-            stream_2 = stream(data).buffer(...).parmap(...).map(...)...
+        data = [...]
+        stream_1, stream_2 = teee(data, 2)
 
-          Upon this setup, we should start "consuming" the two streams at once, letting them run
-          concurrently, and they will also finish around the same time. The concurrent "trigger"
-          needs to be in different threads. Suppose ``stream_1`` is run for some side effect, hence
-          we can simply ``drain`` it; for ``stream_2``, on the other hand, we want its outcoming elements.
-          We may do this::
+       The two streams can be applied the :class:`Stream` operations freely and independently
+       (after all, they are proper ``Stream`` objects), except for the final "trigger" operations
+       :meth:`~Stream.__iter__`, :meth:`~Stream.collect`, and :meth:`~Stream.drain`::
 
-            with concurrent.futures.ThreadPoolExecutor() as pool:
-                t1 = pool.submit(stream_1.drain)
-                t2 = pool.submit(stream_2.collect)
-                n = t1.result()
-                output = t2.result()
+        stream_1.map(...).batch(...).parmap(...)...
+        stream_2.buffer(...).parmap(...).map(...)...
+
+       Upon this setup, we need to start "consuming" the two streams at once, letting them run
+       concurrently, and they will also finish around the same time. The concurrent "trigger"
+       needs to be in different threads. Suppose ``stream_1`` is run for some side effect, hence
+       we can simply ``drain`` it; for ``stream_2``, on the other hand, suppose we need
+       its outcoming elements. We may do this::
+
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            t1 = pool.submit(stream_1.drain)
+            t2 = pool.submit(stream_2.collect)
+            n = t1.result()
+            output = t2.result()
 
     The opposite of ``tee`` is the built-in ``zip``.
+
+    Parameters
+    ----------
+    buffer_size
+        Size of an internal buffer, in terms of the number of elements of ``instream`` it holds.
+        Unless each element takes a lot of memory, it is recommended to use a large buffer size,
+        such as the default 256.
+
+        Think of the buffer as a "moving window" on ``instream``. The ``n`` "forks" produced by
+        this function are iterating over ``instream`` independently subject to one constraint:
+        at any moment, the next element to be obtained by each fork is an element in this window.
+        Once the first element in the window has been obtained by all ``n`` forks, the window will
+        advance by one element. (In detail, the oldest element in the window is dropped; the next
+        element is obtained from ``instream`` and appended to the window as the newest element.)
+
+        This moving window gives the "forks" wiggle room in their consumption of the data---they
+        do not have to be processing the same element at the same time; their respective concurrent
+        processing may be slow at different elements. A larger buffer window reduces the need
+        for the forks to wait for their slower peers (if the slowness is on random elements rather
+        than on every element).
     '''
     assert buffer_size >= 2
     # In practice, use a reasonably large value that is feasible for the application.
--- tests/experiments/test.py
+++ tests/experiments/test.py
@@ -1,10 +1,22 @@
 from mpservice.multiprocessing import Manager
+from mpservice.threading import Thread
+from mpservice.multiprocessing import Process
+from time import sleep
+
+def worker(e):
+    sleep(.1)
+    return e.is_set()
 
 
 def main():
     m = Manager()
     e = m.Event()
+    t = Process(target=worker, args=(e,))
+    t.start()
+
+    t.join()
     m.shutdown()
+    print('shut down')
 
 
 if __name__ == '__main__':
